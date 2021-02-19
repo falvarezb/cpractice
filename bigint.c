9cd67173afcca4c471c417486575851c2725fbbd
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-02-19 23:04:37 +0000

    refactoring

--- bigint.c
+++ bigint.c
@@ -223,34 +223,6 @@ int pad_operands(struct bigint bigint_a, struct bigint bigint_b)
     return EXIT_FAILURE;
 }
 
-int calculate_greatest_operand(struct bigint bigint_a, struct bigint bigint_b)
-{
-    if (bigint_a.num_digits > bigint_b.num_digits)
-    {
-        return 1;
-    }
-    else if (bigint_a.num_digits < bigint_b.num_digits)
-    {
-        return 2;
-    }
-
-    int j = bigint_a.num_digits - 1;
-    while (j >= 0 && bigint_a.number[j] == bigint_b.number[j])
-    {
-        j--;
-    }
-
-    if (j == -1)
-    {
-        return 0;
-    }
-    else if (bigint_a.number[j] > bigint_b.number[j])
-    {
-        return 1;
-    }
-
-    return 2;
-}
 
 /*
     bigint_a >= bigint_b ?
@@ -338,29 +310,3 @@ char *bigsubtract(char *a, char *b)
     free(result_str);
     return signed_result;
 }
-
-// int main(int argc, char const *argv[]) {
-//     // size_t a = 576460752303423488lu;//2**59 (BASE_EXP digits)
-//     // size_t b = 8764607lu;
-//     // size_t u[] = {a,b};
-//     // size_t v[] = {a,b};
-
-//     // int num_digits = fmax(sizeof(u)/sizeof(u[0]), sizeof(v)/sizeof(v[0]));
-//     // size_t w[num_digits+1];
-
-//     // sum(num_digits, u, v, w);
-//     // for (int i = num_digits; i >= 0; i--) {
-//     //     printf("%lu", w[i]);
-//     // }
-//     // printf("\nhello %lu\n", a+a);
-//     // size_t *result = from_string("000000000000000456123456789012345678");
-//     // printf("result=%lu\n", from_string("678")[0]);
-//     // size_t bigint[] = {123456789012345678, 456};
-//     // char* result2 = to_string(result,2);
-//     // printf("%s", result2);
-
-//     char *result = bigsubtract("12", "12");
-//     printf("result=%s\n", result);
-//     return 0;
-//     //0000000000000000456123456789012345678
-// }
